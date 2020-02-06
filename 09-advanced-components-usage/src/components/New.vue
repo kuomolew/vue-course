a4e0ae1fdd56c93ed30124705739b1dca17637b6
kuomolew <kuomolew@gmail.com>
2020-02-06 23:38:40 +0200

    replaced

---
+++ 09-advanced-components-usage/src/components/New.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h3>New Quote</h3>
+        <button @click="increaseCounter">Push the Button</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        methods: {
+            increaseCounter() {
+                this.counter++;
+            }
+        },
+        destroyed() {
+            console.log("They've killed Kenny")
+        },
+        activated() {
+            console.log("They've activated Kenny");
+        },
+        deactivated() {
+            console.log("They've deactivated Kenny");
+        }
+    } 
+</script>
+
+<style scoped>
+    
+</style>

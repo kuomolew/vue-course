d3af713e4fc819f6a1157be2f44feefe0ef220b3
kuomolew <kuomolew@gmail.com>
2021-08-04 11:00:47 +0300

    forms init

---
+++ 136-forms/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <the-form></the-form>
+</template>
+
+<script>
+import TheForm from "./components/TheForm.vue";
+
+export default {
+  name: "App",
+  components: {
+    TheForm,
+  },
+};
+</script>
+
+<style></style>
